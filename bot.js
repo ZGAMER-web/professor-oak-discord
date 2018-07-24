561b92811d2e06e0beaf1a28cb692d9d765eaff1
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-07-24 15:20:27 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on("message", message => {
   if (message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
   
-bot.setPlayingGame("V2.0.0")
+client.setPlayingGame("V2.0.0")
   
   // This is the best way to define args. Trust me.
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
