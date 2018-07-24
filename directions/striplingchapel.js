9410f02ba29fd504b518b0c735ce87db7a762d79
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-07-24 10:53:28 -0400

    Add files via upload

---
+++ directions/striplingchapel.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. I see you need information on the gym **Stripling Chapel**. My research shows: https://goo.gl/maps/1J5y2STrdYB2").catch(console.error);
+}
---
+++ directions/tanner.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("Hello, Trainer. I see you need information on the gym **Tanner Medical Center Med Library**. My research shows: https://goo.gl/maps/udWG78cw3VU2").catch(console.error);
+}
