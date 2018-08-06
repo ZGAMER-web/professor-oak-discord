7bdc4740e23f7ef59c734464d62319e267d18269
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-06 16:15:42 -0400

    Update helpme.js

--- commands/helpme.js
+++ commands/helpme.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
 message.channel.send('Hello Trainer! What do you need help with? You can say one something like **Directions**.')
 .then(() => {
-  message.channel.awaitMessages(response => response.content === 'directions' 'silph road', {
+  message.channel.awaitMessages(response => response.content === 'directions','silph road', {
     max: 1,
     time: 30000,
     errors: ['time'],
