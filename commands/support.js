f4f4f4c5fab4e102156d6dd5454a72c3cca559a4
mrerecordholder <>
2018-08-05 21:35:48 -0400

    v.3.0.0 support update

---
+++ commands/support.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("<@&471363796875739157> A Trainer is requesting your assistance in this channel. Please see above...").catch(console.error);
+}
