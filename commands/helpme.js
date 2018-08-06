c1fd42e556db6682123b211e7ee6a7c9de9c8a65
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-06 16:06:44 -0400

    Update helpme.js

--- commands/helpme.js
+++ commands/helpme.js
@@ -1,13 +1,13 @@
 exports.run = (client, message, args) => {
-message.channel.send('What tag would you like to see? This will await will be cancelled in 30 seconds. It will finish when you provide a message that goes through the filter the first time.')
+message.channel.send('Hello Trainer! What do you need help with? You can say one something like **Directions**.')
 .then(() => {
-  message.channel.awaitMessages(response => response.content === 'test', {
+  message.channel.awaitMessages(response => response.content === 'directions', {
     max: 1,
     time: 30000,
     errors: ['time'],
   })
   .then((collected) => {
-      message.channel.send(`The collected message was: ${collected.first().content}`);
+      message.channel.send(`!oak ${collected.first().content}`);
     })
     .catch(() => {
       message.channel.send('There was no collected message that passed the filter within the time limit!');
