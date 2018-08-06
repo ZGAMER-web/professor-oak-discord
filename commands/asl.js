0bd4718ed79867de7a0f14bc9667bfb0bdec86ae
mrerecordholder <>
2018-08-06 12:42:34 -0400

    test

--- commands/asl.js
+++ commands/asl.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
-    let age = args[0]; // Remember arrays are 0-based!.
-    let sex = args[1];
-    let location = args[2];
-    message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
-    message.send('Test This');
+    const swearWords = ["darn", "shucks", "frak", "shite"];
+if( swearWords.some(word => message.content.includes(word)) ) {
+  message.reply("Oh no you said a bad word!!!");
+  // Or just do message.delete();
+}
 }
