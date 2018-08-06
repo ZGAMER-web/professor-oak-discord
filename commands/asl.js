f95fa5ccef1404c332529c0646bc5393aad89059
mrerecordholder <>
2018-08-06 12:32:26 -0400

    bug fix

--- commands/asl.js
+++ commands/asl.js
@@ -2,5 +2,5 @@ exports.run = (client, message, args) => {
     let age = args[0]; // Remember arrays are 0-based!.
     let sex = args[1];
     let location = args[2];
-    message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);}
+    message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
 }
