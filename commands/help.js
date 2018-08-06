7c0acccf90f7b25089df6de49ca41a9128df1328
mrerecordholder <>
2018-08-05 20:59:27 -0400

    v.3.0.0 update

--- commands/help.js
+++ commands/help.js
@@ -5,46 +5,18 @@ exports.run = (client, message, args) => {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    description: "Hello Trainer, Below is a list of commands I have learned through my research. To execute, type any command below exactly how they appear.",
+    description: "Hello Trainer, I see you need a little help. To get directions, use `!oak` followed by the name of a Gym or Pokestop *(one word, no spaces)*. You can use the full name, short name or nickname. The word *uwg* and *church* can be removed from any Gym name. I have listed a few examples below.",
     fields: [{
-        name: "!oak help",
-        value: "Will display this message *(list of commands)*."
+        name: "!oak floralbench",
+        value: "Directions to the Gym, Floral Bench"
       },
       {
-        name: "!oak iv",
-        value: "Get information on what Pokemon IV's are and how to check them."
+        name: "!oak welcomehill",
+        value: "Directions to the Gym, Welcome Hill Baptist Church."
       },
       {
-        name: "!oak *pokemon* (replace *pokemon* with pokemon name)",
-        value: "Valuable information about a specific Pokemon, use `!oak pokemonlist` for available Pokemon."
-      },
-      {
-        name: "!oak eeveetrick",
-        value: "Discover how to evolve an Eevee into each eevolution use name and walking tricks."
-      },
-      {
-        name: "!oak alolan",
-        value: "Displays all available Alolan forms of the Kanto region Pokemon."
-      },
-      {
-        name: "!oak raidtimes",
-        value: "Returns when raids start spawning and when they despawn."
-      },
-      {
-        name: "!oak nestatlas",
-        value: "This map shows traveling and local Trainers worldwide nests."
-      },
-      {
-        name: "!oak leaguemap",
-        value: "This map helps trainers connect with communities worldwide."
-      },
-      {
-        name: "!oak googlemap",
-        value: "A map created by our community that shows all Gyms, Ex Raid eligable Gyms, Pokestops, in-game parks and water spawn locations."
-      },
-      {
-        name: "!oak *gym* (replace *gym* with gym name, no spaces)",
-        value: "General information about a specific gym along with map directions. Use `!oak gymlist` for available gyms."
+        name: "!oak acetrainer",
+        value: "I will let the Ace Trainer's know you need further help or that there is a issue."
       },
     ],
     timestamp: new Date(),
