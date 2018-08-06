00e6d133b177176a2f32390e7f6bdbb4628c78eb
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-06 16:14:20 -0400

    Update helpme.js

--- commands/helpme.js
+++ commands/helpme.js
@@ -1,16 +1,16 @@
 exports.run = (client, message, args) => {
 message.channel.send('Hello Trainer! What do you need help with? You can say one something like **Directions**.')
 .then(() => {
-  message.channel.awaitMessages(response => response.content === 'directions', {
+  message.channel.awaitMessages(response => response.content === 'directions' 'silph road', {
     max: 1,
     time: 30000,
     errors: ['time'],
   })
   .then((collected) => {
-      message.channel.send(`!oak ${collected.first().content}`);
+      message.channel.send(`I see you need help with ${collected.first().content}`);
     })
     .catch(() => {
-      message.channel.send('There was no collected message that passed the filter within the time limit!');
+      message.channel.send('My time here is done Trainer! If you need more help just type **!oak help again**. I am going to do more research.');
     });
 });
 }
