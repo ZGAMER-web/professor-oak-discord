9b37e5880788c98eb7c1b6259a860290216c2f56
mrerecordholder <>
2018-08-05 21:43:13 -0400

    v.3.0.0

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,7 @@
 exports.run = (client, message, args) => {
     message.channel.send({embed: {
     color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    description: "Hello Trainer, I see you need a little help. To get directions, use `!oak` followed by the name of a Gym or Pokestop *(one word, no spaces)*. You can use the full name, short name or nickname. The word *uwg* and *church* can be removed from any Gym name. I have listed a few examples below.",
+    description: "Hello Trainer, I see you need a little help. To get directions, use `!oak` followed by the name of a Gym *(one word, no spaces)*. You can use the full name, short name or nickname. The word *uwg* and *church* can be removed from any Gym name. I have listed a few examples below.",
     fields: [{
         name: "!oak floralbench",
         value: "Directions to the Gym, Floral Bench"
