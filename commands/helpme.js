56154ff0dd7f782176322ca0b14fa037d4165294
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-06 16:00:48 -0400

    Create helpme.js

---
+++ commands/helpme.js
@@ -0,0 +1,16 @@
+exports.run = (client, message, args) => {
+message.channel.send('What tag would you like to see? This will await will be cancelled in 30 seconds. It will finish when you provide a message that goes through the filter the first time.')
+.then(() => {
+  message.channel.awaitMessages(response => response.content === 'test', {
+    max: 1,
+    time: 30000,
+    errors: ['time'],
+  })
+  .then((collected) => {
+      message.channel.send(`The collected message was: ${collected.first().content}`);
+    })
+    .catch(() => {
+      message.channel.send('There was no collected message that passed the filter within the time limit!');
+    });
+});
+}
