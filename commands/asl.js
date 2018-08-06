49d9ec87c505dd383cb7fc52719f130608638f19
mrerecordholder <>
2018-08-06 12:37:33 -0400

    test update

--- commands/asl.js
+++ commands/asl.js
@@ -3,4 +3,5 @@ exports.run = (client, message, args) => {
     let sex = args[1];
     let location = args[2];
     message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
+    message.send('Test This');
 }
