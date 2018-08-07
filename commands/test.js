e90cb8ab0afa32d969aadf2a7bcbaab8918f5a13
mrerecordholder <>
2018-08-06 22:28:05 -0400

    v3.1.1 test

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
     const gym = ["ag center"];
-    message.channel.send(gym).catch(console.error);
-    message.delete(2000)
+    message.channel.send(gym)("is located").catch(console.error);
+    message.delete(3000)
 } 
