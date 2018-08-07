3f68b6cdf0d37879a9c8ea2a53173203180ade36
mrerecordholder <>
2018-08-07 00:09:29 -0400

    test update

--- commands/test.js
+++ commands/test.js
@@ -1,12 +1,12 @@
 exports.run = (client, message, args) => {
-    const greeting = "Hello Trainer! Based on my research ";
+    const greeting = "Hello Trainer! I see you need information on ";
     
-    const gym = "**Gym Name**"; // gym name here
-    const exraid = "is"; // response should be "is" or "is not"
-    const parking = "free parking";
+    const loc = "Gym" // gym or pokestop
+    const locname = "**East Carroll**"; // gym or pokestop name
+    const access = "easy" // easy, diffucult, etc..
 
-    const gymresponse = greeting + gym +  exraid + " Ex-Raid eligable.";
+    const gymresponse = greeting + " the " + location + " called " + locname + ". Based on my research, I've found that " + "access is " + access;
 
     message.channel.send(gymresponse).catch(console.error);
-    message.delete(3000)
+    message.channel.delete(5000)
 } 
