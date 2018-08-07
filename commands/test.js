09c46165be3d4719461fb315aeb57a8a0abcfd42
mrerecordholder <>
2018-08-07 00:11:49 -0400

    bug fix

--- commands/test.js
+++ commands/test.js
@@ -1,9 +1,9 @@
 exports.run = (client, message, args) => {
     const greeting = "Hello Trainer! I see you need information on ";
     
-    const loc = "Gym" // gym or pokestop
+    const loc = "Gym"; // gym or pokestop
     const locname = "**East Carroll**"; // gym or pokestop name
-    const access = "easy" // easy, diffucult, etc..
+    const access = "easy"; // easy, diffucult, etc..
 
     const gymresponse = greeting + " the " + location + " called " + locname + ". Based on my research, I've found that " + "access is " + access;
 
