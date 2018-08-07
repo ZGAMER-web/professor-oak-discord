2c8bf99cddffcbec71ed780c06721c47b66e9a0f
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-07 09:25:47 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
+    import * as myConstClass from '/data/gyms';
     
-    const greeting = "Hello Trainer! Based on my Gym research:"; // default message to start the reply
     const gym = "**East Carroll**"; // gym name
     const location = "Carrollton"; // city
     const exraid = "it is"; // it is or it isn't
@@ -8,7 +8,7 @@ exports.run = (client, message, args) => {
     const notes = ""; // optional notes about the gym
     const map = "https://goo.gl/maps/YuNoQopXHqJ2"; // google map link
 
-    const gymresponse = greeting + " " + gym + " is located in " + location + ", access is " + access + " and " + exraid + " Ex Raid eligable. " + notes + " " + map;
+    const gymresponse = GYM_GREETING + " " + gym + " is located in " + location + ", access is " + access + " and " + exraid + " Ex Raid eligable. " + notes + " " + map;
 
     message.channel.send(gymresponse).catch(console.error);
     message.delete()
