ac41855ad38ffba966550795307c6be055e8741a
mrerecordholder <>
2018-08-06 23:33:05 -0400

    test

--- commands/test.js
+++ commands/test.js
@@ -1,8 +1,11 @@
 exports.run = (client, message, args) => {
-    const greeting = "Hello Trainer!";
-    const gym = "ag center";
+    const greeting = "Hello Trainer! Based on my research ";
+    
+    const gym = "**Gym Name**"; // gym name here
+    const exraid = "is"; // response should be "is" or "is not"
+    const parking = "free parking";
 
-    const gymresponse = greeting + " I see you need directions to " + gym;
+    const gymresponse = greeting + gym + exraid + " Ex-Raid eligable.";
 
     message.channel.send(gymresponse).catch(console.error);
     message.delete(3000)
