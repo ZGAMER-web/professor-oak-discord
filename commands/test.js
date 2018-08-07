ed30ae56ac0551cd5946895515c7cd1e771fd695
mrerecordholder <>
2018-08-06 23:06:28 -0400

    test

--- commands/test.js
+++ commands/test.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
-    const greeting = ["Hello Trainer!"]
-    const gym = ["ag center"];
-    message.channel.send((greeting), "I see you need directions to", (gym)).catch(console.error);
+    const greeting = "Hello Trainer!",
+    const gym = "ag center";
+    message.channel.send((greeting) + " I see you need directions to " + (gym)).catch(console.error);
     message.delete(3000)
 } 
