cc6b0cbc5f55791e65f2cfc7d8793d5f1a7eebd0
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-07 16:12:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const prefix = config.prefix
 const fs = require("fs");
 const responderObject = require('./data/responder.json')
 
-
+// LOADS & RUNS THE COMMAND FOLDER
 client.on("message", message => {
   if (message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
@@ -23,10 +23,19 @@ client.on("message", message => {
   
 });
 
-
+// RESPONDER
 client.on("message", (message) => {
   if(responderObject[message.content]) {
     message.channel.send(responderObject[message.content]);
+  }
+});
+
+// INDIVIDUAL COMMANDS
+client.on("message", (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(config.prefix + "googlemap")) {
+    message.channel.send("Hello, Trainer. Long research has went into the Google Map. It now displays Gyms, Ex Raid eligable Gyms, Pokestops, in-game parks, and water spawns. You can now save the map to your Google account but clicking on the link below, then signing in on the top right. Close the browser, open Google Maps app, click the menu button, your places, then click the *Pokemon Go Carrollton Map*. https://drive.google.com/open?id=1rgYwn9FzHC1G1Ir4SCGbO4I_JeJLkHKb&usp=sharing");
   }
 });
 
