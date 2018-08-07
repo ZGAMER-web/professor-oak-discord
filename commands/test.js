712210d1ddbfdf1022b2f1ee88dd709b9c6a5e88
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-07 10:28:48 -0400

    v.3.2.0

--- commands/test.js
+++ commands/test.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args) => {
     const GYM_NAME = "**East Carroll**";
     const GYM_LOCATION = "Carrollton Georgia"; // "City State"
     const EX_RAID = "it is"; // "it is" or "it is not"
-    const GYM_ACCESS = "easy"; // "easy" "easy with free parking" "diffucult" "diffucult with paid parking"
+    const GYM_ACCESS = "easy with free parking"; // "easy" "easy with free parking" "diffucult" "diffucult with paid parking"
     const GYM_NOTES = ""; // optional notes about the gym
     const MAP_LINK = "https://goo.gl/maps/YuNoQopXHqJ2"; // google map link
     // ------- EDIT THE ABOVE FIELDS ONLY
