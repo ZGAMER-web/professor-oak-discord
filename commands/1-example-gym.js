975bd8d70e0f830ec1fef5ea7e82c10ec78a1aaa
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-07 10:33:29 -0400

    v.3.2.0

--- commands/1-example-gym.js
+++ commands/1-example-gym.js
@@ -1,3 +1,32 @@
 exports.run = (client, message, args) => {
-    message.channel.send("Hello, Trainer. I see you need information on the gym ****. My research shows: vehicle.").catch(console.error);
-}
+    
+    // DEFAULT GYM GREETING, DO NOT CHANGE!
+    const GYM_GREETING = "Hello Trainer! According to my research,"
+    
+    
+    // ------- EDIT THE BELOW FIELDS ONLY
+    const GYM_NAME = "****";
+    
+    // City State
+    const GYM_LOCATION = "Carrollton Georgia"; 
+    
+    // "it is" or "it is not"
+    const EX_RAID = "it is not";
+    
+    // "easy" "easy with free parking" "diffucult" "diffucult with paid parking"
+    const GYM_ACCESS = "easy with free parking"; 
+    
+    // optional notes about the gym
+    const GYM_NOTES = ""; 
+    
+    // google map link
+    const MAP_LINK = "https://goo.gl/maps/YuNoQopXHqJ2"; 
+    // ------- EDIT THE ABOVE FIELDS ONLY
+    
+    
+    // FULL RESPONSE, DO NOT CHANGE!
+    const GYM_RESPONSE = GYM_GREETING + " " + GYM_NAME + " is a Gym located in " + GYM_LOCATION + ", access is " + GYM_ACCESS + " and " + EX_RAID + " Ex Raid eligable. " + GYM_NOTES + " " + MAP_LINK;
+
+    message.channel.send(GYM_RESPONSE).catch(console.error);
+    message.delete()
+} 
