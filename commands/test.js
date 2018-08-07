3e6f2957c59c84a00b7c62f7a9912a5caa14ebae
mrerecordholder <>
2018-08-07 00:49:09 -0400

    update

--- commands/test.js
+++ commands/test.js
@@ -1,15 +1,15 @@
 exports.run = (client, message, args) => {
-    message.delete(5000)
     
-    const greeting = "Hello Trainer! Based on my Gym research,"; // default message to start the reply
+    const greeting = "Hello Trainer! Based on my Gym research:"; // default message to start the reply
     const gym = "**East Carroll**"; // gym name
     const location = "Carrollton"; // city
     const exraid = "it is"; // it is or it isn't
     const access = "easy"; // easy, diffucult, etc..
+    const notes = ""; // optional notes about the gym
     const map = "https://goo.gl/maps/YuNoQopXHqJ2"; // google map link
 
-    const gymresponse = greeting + " " + gym + " is located in " + location + ", access is " + access + " and " + exraid + " Ex Raid eligable. " + map;
+    const gymresponse = greeting + " " + gym + " is located in " + location + ", access is " + access + " and " + exraid + " Ex Raid eligable. " + notes + " " + map;
 
     message.channel.send(gymresponse).catch(console.error);
-    
+    message.delete()
 } 
