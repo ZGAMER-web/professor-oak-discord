2ea5428834f45db888d9a972fc55d8e05176938a
mrerecordholder <>
2018-08-07 00:38:22 -0400

    update

--- commands/test.js
+++ commands/test.js
@@ -2,11 +2,12 @@ exports.run = (client, message, args) => {
     
     const greeting = "Hello Trainer! Based on my Gym research,"; // default message to start the reply
     const gym = "**East Carroll**"; // gym name
-    const exraid = "is"; // is or is not ex raid eligable
+    const location = "Carrollton"; // city
+    const exraid = "it is"; // it is or it isn't
     const access = "easy"; // easy, diffucult, etc..
     const map = "https://goo.gl/maps/YuNoQopXHqJ2"; // google map link
 
-    const gymresponse = greeting + " " + gym + " " + exraid + " Ex Raid eligable" + " and access is " + access + "." + " Here is directions in case you need them" + map;
+    const gymresponse = greeting + " " + gym + " is located in " + location + " ,access is " + access + " and " + exraid + " Ex Raid eligable. " + map;
 
     message.channel.send(gymresponse).catch(console.error);
     message.delete(5000)
