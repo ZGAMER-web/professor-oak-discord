da2aa90af11d30faa16bb9506dfb603ac303961a
mrerecordholder <>
2018-08-06 22:57:23 -0400

    test

--- commands/test.js
+++ commands/test.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     const greeting = ["Hello Trainer!"]
     const gym = ["ag center"];
-    message.channel.send(greeting)(gym).catch(console.error);
+    message.channel.send((greeting), "I see you need directions to", (gym)).catch(console.error);
     message.delete(3000)
 } 
