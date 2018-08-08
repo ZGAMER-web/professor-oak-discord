f7ef5256f7362eaf3e66ceb51dbf2c23471450ff
mrerecordholder <>
2018-08-07 21:04:22 -0400

    v3.2.0 test

--- commands/test2.js
+++ commands/test2.js
@@ -1,17 +1,17 @@
 module.exports.run = (client, message, args) => {
     const Discord = require("discord.js");
   const embed = new Discord.RichEmbed()
-
-  const GYM_NAME = " Skate Park"
   
-    .setTitle("East Carroll" + GYM_NAME)
+    .setTitle("East Carroll")
     
     .setColor(0x00AE86)
-    .setDescription("Hello Trainer! Based on my research, I have found the following.")
+    .setDescription("[CLICK HERE FOR DIRECTIONS](https://maps.google.com/maps?ll=33.599598,-85.044185&q=33.599598,-85.044185%20(East%20Carroll%20Park))")
     .setFooter("© Professor Oak", "https://raw.githubusercontent.com/MrRecordHolder/professor-oak-discord/master/images/oak1.jpg")
+
     .setThumbnail("https://lh3.ggpht.com/TezUfbTmNcOZheSzLcq2Cfen7TSHCHAQKMS0nO0OYRh1X4Zzljer9QuPOouW5TAKPiPnI65WsCWcPQR5JreN")
     
     .addField("Location", "Carrollton, GA", true)
+    .addField("Access", "Easy, Free Parking", true)
     .addField("Ex Raid Eligable", "Yes", true)
 
     .setTimestamp()
