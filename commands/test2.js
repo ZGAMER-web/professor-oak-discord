ff7f7333b4d73bdf20cfecd87b8236765edb6f22
mrerecordholder <>
2018-08-07 21:11:20 -0400

    update

--- commands/test2.js
+++ commands/test2.js
@@ -2,17 +2,18 @@ module.exports.run = (client, message, args) => {
     const Discord = require("discord.js");
   const embed = new Discord.RichEmbed()
   
-    .setTitle("East Carroll")
+  .setColor(0x4A90E2)
+  .setTitle("East Carroll")
+  .setDescription("[CLICK HERE FOR DIRECTIONS](https://goo.gl/maps/q8qup3vJkrt)
+  .setThumbnail("https://lh3.ggpht.com/TezUfbTmNcOZheSzLcq2Cfen7TSHCHAQKMS0nO0OYRh1X4Zzljer9QuPOouW5TAKPiPnI65WsCWcPQR5JreN")
     
-    .setColor(0x00AE86)
-    .setDescription("[CLICK HERE FOR DIRECTIONS](https://maps.google.com/maps?ll=33.599598,-85.044185&q=33.599598,-85.044185%20(East%20Carroll%20Park))")
-    .setFooter("© Professor Oak", "https://raw.githubusercontent.com/MrRecordHolder/professor-oak-discord/master/images/oak1.jpg")
+    .addField("Location", "Carrollton, GA",)
+    .addBlankField(true)
+    .addField("Access", "Easy, Free Parking",)
+    .addBlankField(true)
+    .addField("Ex Raid Eligable","Yes", true)
 
-    .setThumbnail("https://lh3.ggpht.com/TezUfbTmNcOZheSzLcq2Cfen7TSHCHAQKMS0nO0OYRh1X4Zzljer9QuPOouW5TAKPiPnI65WsCWcPQR5JreN")
-    
-    .addField("Location", "Carrollton, GA", true)
-    .addField("Access", "Easy, Free Parking", true)
-    .addField("Ex Raid Eligable", "Yes", true)
+    .setFooter("© Professor Oak", "https://raw.githubusercontent.com/MrRecordHolder/professor-oak-discord/master/images/oak1.jpg")
 
     .setTimestamp()
   
