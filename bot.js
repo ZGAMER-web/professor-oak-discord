cea2e77a0203665d90eda638951809bec7e133d3
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-08 10:07:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,8 +17,7 @@ client.on("message", message => {
  
   //CHECK FOR COMMANDS
   try {
-    let commandFile = require(`./commands/${command}.js`);
-    let commandFile = require(`./pokestops/${command}.js`);
+    let commandFile = require(`./commands/${command}.js`,(`./pokestops/${command}.js`));
     commandFile.run(client, message, args);
   } catch (err) {
     console.error(err);
