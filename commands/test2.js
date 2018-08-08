72cbfce2bf36619178c943a6cbfa35abb677a60c
mrerecordholder <>
2018-08-07 21:13:53 -0400

    bug fix

--- commands/test2.js
+++ commands/test2.js
@@ -7,11 +7,11 @@ module.exports.run = (client, message, args) => {
   .setDescription("[CLICK HERE FOR DIRECTIONS](https://goo.gl/maps/q8qup3vJkrt)
   .setThumbnail("https://lh3.ggpht.com/TezUfbTmNcOZheSzLcq2Cfen7TSHCHAQKMS0nO0OYRh1X4Zzljer9QuPOouW5TAKPiPnI65WsCWcPQR5JreN")
     
-    .addField("Location", "Carrollton, GA",)
+    .addField("Location", "Carrollton, GA")
     .addBlankField(true)
-    .addField("Access", "Easy, Free Parking",)
+    .addField("Access", "Easy, Free Parking")
     .addBlankField(true)
-    .addField("Ex Raid Eligable","Yes", true)
+    .addField("Ex Raid Eligable","Yes")
 
     .setFooter("© Professor Oak", "https://raw.githubusercontent.com/MrRecordHolder/professor-oak-discord/master/images/oak1.jpg")
 
