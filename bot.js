2df3cd127de1f3aa16a9192489d36685e126b2a2
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-08 14:31:16 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,10 +8,10 @@ const responderObject = require('./data/responder.json');
 // LOADS & RUNS THE COMMAND FOLDER
 client.on("message", message => {
   if (message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
+  if(message.content.indexOf(prefix) !== 0) return;
   client.user.setGame("!oak v.3.1.2");
    
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
  
