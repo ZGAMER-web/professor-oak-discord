7b199ce3591271390c1e025e3c880cfcc4562af6
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-09 14:17:40 -0400

    Update and rename help1.js to help.js

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,4 @@
+exports.run = (client, message, args) => {
 message.channel.send('Hello, Trainer! What do you need help with? You can respond with single words as shown below... \n **directions** - Learn how to get directions to Gyms and Pokestops.')
 .then(() => {
   message.channel.awaitMessages(response => response.content === 'directions', {
