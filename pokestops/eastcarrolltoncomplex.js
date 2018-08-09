9591f7b2f7b3d15b4bd13573d93880f9ffd7797a
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-09 14:48:38 -0400

    Create eastcarrolltoncomplex.js

---
+++ pokestops/eastcarrolltoncomplex.js
@@ -0,0 +1,22 @@
+module.exports.run = (client, message, args) => {
+    const Discord = require("discord.js");
+  const embed = new Discord.RichEmbed()
+  
+    .setTitle("East Carrollton Softball/Baseball Complex") // POKESTOP NAME
+    
+    .setColor(0x00AE86)
+
+    
+    .setDescription("[CLICK HERE FOR DIRECTIONS](https://goo.gl/maps/fzkWBdZpmjz)") // GOOGLE MAP LINK INSIDE ()
+    .setFooter("© Professor Oak", "https://raw.githubusercontent.com/MrRecordHolder/professor-oak-discord/master/images/oak1.jpg")
+    .setThumbnail("https://lh6.ggpht.com/p0OQQBwT8Uft8W_yutFRQjvnyFfAr9F0a0Quyvw-kAORvOuKxEC6v-CBchVuVQv6kzEBRuKtJpQukp5lqfG8") // INGRESS PORTAL IMAGE
+    
+    .addField("**Location**", "Carrollton, GA")
+    .addField("**Access**", "Easy")
+    .addField("**Nested**", "No")
+    .addField("**Commands**", "`eastcarrolltoncomplex`") // COMMAND NAME INSIDE ``
+
+    .setTimestamp()
+  
+    message.channel.send({embed});
+  }
