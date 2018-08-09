a263b9608c61ae766a2a2f286c1fa4cbc2d81acc
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-09 16:46:22 -0400

    Update oakchart.js

--- commands/oakchart.js
+++ commands/oakchart.js
@@ -5,7 +5,7 @@ module.exports.run = (client, message, args) => {
     .setTitle("**Professor Oak Commands**")
     
     .setColor(0x00AE86)
-    .setDescription("Get directions or game information using bot commands. Must use **!oak** for me to recognize your talking to me. This channel is cleared frequently to keep it squeaky clean. Happy exploring, Trainers!")
+    .setDescription("I can help with Gym & Pokestop directions along with other game features I've studied. For me to recognize your talking to me, start each new message with **!oak**. This channel is cleared frequently to keep it squeaky clean. See the chart below for reference, for help just ask in general chat. Happy exploring, Trainers!")
     .setFooter("© Professor Oak", "https://raw.githubusercontent.com/MrRecordHolder/professor-oak-discord/master/images/oak1.jpg")
     .setThumbnail("https://vignette.wikia.nocookie.net/pokemon/images/8/84/Professor_Oak_XY.png/revision/latest?cb=20131221014829")
     .setImage("https://github.com/MrRecordHolder/professor-oak-discord/blob/master/images/charts/professor-oak-bot-chart.png?raw=true")
