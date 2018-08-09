6784a206d8dcc2c499e9f6ded9fc1b426db2461c
mrerecordholder <>
2018-08-08 23:05:35 -0400

    verbage fix

--- commands/questchart.js
+++ commands/questchart.js
@@ -2,10 +2,10 @@ module.exports.run = (client, message, args) => {
     const Discord = require("discord.js");
   const embed = new Discord.RichEmbed()
   
-    .setTitle("**August Field Research, Daily Reportings**")
+    .setTitle("**Daily Field Research Reportings**")
     
     .setColor(0x00AE86)
-    .setDescription("Report Field Research tasks that you've picked up from Pokestops. See the chart below for reference. This channel is cleared daily. Happy exploring, Trainers.**")
+    .setDescription("Report Field Research tasks that you've picked up from Pokestops. See the chart below for reference. This channel is cleared daily. Happy exploring & reporting, Trainers!")
     .setFooter("© Professor Oak", "https://raw.githubusercontent.com/MrRecordHolder/professor-oak-discord/master/images/oak1.jpg")
     .setThumbnail("https://vignette.wikia.nocookie.net/pokemongo/images/8/84/Mp-Icon_Research.png/revision/latest?cb=20180328113134")
     .setImage("https://raw.githubusercontent.com/MrRecordHolder/professor-oak-discord/master/images/charts/field-research-aug.png")
