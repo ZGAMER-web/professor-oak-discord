4f60aebe5aa7339cc02c6cba2b1d59800cbefd8d
mrerecordholder <>
2018-08-26 16:54:10 -0400

    beta

---
+++ roles/nn.js
@@ -0,0 +1,4 @@
+if (message.content.includes('changeNick')) {
+    if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
+    message.member.setNickname(message.content.replace('changeNick ', ''));
+}
