7e0a42ac43eb9c4cb796e306cea4f4625b25da4e
mrerecordholder <>
2018-08-25 21:18:46 -0400

    update

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,5 +1,11 @@
 client.on('message', (message) => {
-    if (message.author.bot) return;
-    var Role=message.guild.roles.find('name','sad')
-    message.member.addRole(Role)
+    let role = message.guild.roles.find("name", "Team Mystic");
+
+    let member = message.members;
+
+    // Add the role!
+    member.addRole(role).catch(console.error);
+
+    // Remove a role!
+    member.removeRole(role).catch(console.error);
 });
