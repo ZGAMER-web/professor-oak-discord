51a60672e83f2ea1ac3d19a6ba6dfa3a6bdaec90
mrerecordholder <>
2018-08-25 21:41:43 -0400

    test

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,12 +1,10 @@
 exports.run = (client, message, args) => {
-        let role = message.guild.roles.find("name", "carrollton" "temple");
+        let role = message.guild.roles.find("name", "carrollton" or "temple");
 
-    // Let's pretend you mentioned the user you want to add a role to (!addrole @user Role Name):
     let member = message.member;
     
-    // or the person who made the command: let member = message.member;
     
     // Add the role!
     member.addRole(role).catch(console.error);
-    message.channel.send("You've been added to " + role + " ,Trainer!");
+    message.channel.send("You've been added, Trainer!");
 };
