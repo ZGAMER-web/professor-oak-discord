c86fbcc889e8247f9bfa4ceea20d6f85f7534d8e
mrerecordholder <>
2018-08-26 13:14:38 -0400

    beta test

--- roles/carrollton.js
+++ roles/carrollton.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
     let role = message.guild.roles.find("name", "carrollton");
     
     let member = message.member;
-    let team_emoji = data.emoji.mystic_emoji
+    let team_emoji = emoji.mystic_emoji
     
     // Add the role!
     member.addRole(role).catch(console.error);
