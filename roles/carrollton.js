6e17e28efdfc3c4253429a1e63db624085ce7b70
mrerecordholder <>
2018-08-26 13:22:06 -0400

    beta test: emoji

--- roles/carrollton.js
+++ roles/carrollton.js
@@ -1,10 +1,8 @@
 exports.run = (client, message, args) => {
     let role = message.guild.roles.find("name", "carrollton");
+    let mystic_emoji = client.emojis.find("name", "mystic");
     
     let member = message.member;
-
-    const team_emoji = require('./data/emoji.json');
-    let team_emoji = emoji.mystic_emoji
     
     // Add the role!
     member.addRole(role).catch(console.error);
@@ -13,5 +11,5 @@ exports.run = (client, message, args) => {
     // Remove a role!
     member.removeRole(role).catch(console.error);
 
-    message.channel.send("Trainer, I've updated your access to the **" + role + " **area. " + team_emoji);
+    message.channel.send("Trainer, I've updated your access to the **" + role + " **area. " + mystic_emoji);
 };
