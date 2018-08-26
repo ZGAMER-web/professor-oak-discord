cb0a505601b41da30df7754de95f926f6bd8d3de
mrerecordholder <>
2018-08-26 16:55:35 -0400

    beta

--- roles/nn.js
+++ roles/nn.js
@@ -1,4 +1,6 @@
-if (message.content.includes('changeNick')) {
-    if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
-    message.member.setNickname(message.content.replace('changeNick ', ''));
-}
+client.on('message', message => {
+    if (message.content.includes('changeNick')) {
+        if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
+        message.member.setNickname(message.content.replace('changeNick ', ''));
+    }
+});
