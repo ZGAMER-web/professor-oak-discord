08b21f8df702f41ffc892c76bf2829e6a7aa155b
mrerecordholder <>
2018-08-26 14:09:20 -0400

    beta test

--- roles/addmystic.js
+++ roles/addmystic.js
@@ -6,5 +6,5 @@ exports.run = (client, message, args) => {
     
     member.addRole(role).catch(console.error);
     
-    message.channel.send("${user}, I've set your team to " + team_emoji + ". You now have access to " + client.channels.get("451397641025355776"));
+    message.channel.send(message.author.toString() + ", I've set your team to " + team_emoji + ". You now have access to " + client.channels.get("451397641025355776"));
 };
