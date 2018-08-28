929a3afa880756e50870fc0565866dca0048f856
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-28 15:15:15 -0400

    Create dm.js

---
+++ commands/dm.js
@@ -0,0 +1,10 @@
+exports.run = (client, message, args) => {
+// makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    const trainer = message.mentions.members.first()
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.trainer.send(sayMessage);
+}
