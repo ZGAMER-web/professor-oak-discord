3ddf46be44f6a49fcf346cf0de81cf60ebf39386
mrerecordholder <>
2018-08-27 23:31:20 -0400

    patch

--- roles/mystic.js
+++ roles/mystic.js
@@ -6,5 +6,5 @@ exports.run = (client, message, args) => {
     
     member.addRole(role).catch(console.error);
     
-    message.channel.get(483420052335362058).send(message.author.toString() + ", You've joined **Team Mystic**" + team_emoji + ". You now have access to your team chat, which is great for taking gyms and earning extra ball bonuses during raids. **Next Step** is to set your location so you can see the raid, quest and sightings channels for your area. I've unlocked the <#483420052335362058> channel for you to do so. For help view the <#483420019854934016> channel.");
+    client.channels.get(483420052335362058).send(message.author.toString() + ', Welcome Trainer!');
 };
