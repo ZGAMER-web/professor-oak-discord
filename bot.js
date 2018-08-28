0dd22c503c995a347c22b5e92c03b5471aeb3679
mrerecordholder <>
2018-08-27 20:21:17 -0400

    v.3.2.3 beta

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const responderObject = require('./data/responder.json');
 client.on("message", message => {
   if (message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
-  client.user.setGame('!oak v.3.2.2');
+  client.user.setGame('!oak v.3.2.3 beta');
    
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
@@ -48,6 +48,18 @@ client.on("message", (message) => {
   if(responderObject[message.content]) {
     message.channel.send(responderObject[message.content]);
   }
+});
+
+
+/// WELCOME NEW TRAINER
+bot.on('guildMemberAdd', member => {
+  member.guild.channels.get('483792183346987009').send('**' + member.user.username + '**, Welcome Trainer! My name is *Professor Oak*, A Discord Bot. To interact with me, you must start each __new__ message with **!Oak** and then a space, followed by a command. \n\n So first, lets set your team! Reply with your team command only.\n*example* **!Oak mystic** or **!oak valor** or **!oak instinct**\n*For help refer to the <#483420019854934016> channel.*'); 
+});
+
+/// TRAINER HAS LEFT THE SEVER
+bot.on('guildMemberRemove', member => {
+  member.guild.channels.get('483792183346987009').send('**' + member.user.username + '**, has left the server.');
+  //
 });
 
 client.login(process.env.BOT_TOKEN);
