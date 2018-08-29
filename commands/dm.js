ec83b4a83847549b3971b1183e96b4ba5112633f
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-29 09:01:04 -0400

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -5,6 +5,5 @@ exports.run = (client, message, args) => {
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
-    message.mentions.members.first(sayMessage);
-    message.member.send(sayMessage);
+    message.mentions.users.first().send("Hey there Trainer!")
 }
