6715da4099dc1a5790940314894c3bfbf580db33
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-29 11:13:38 -0400

    Update dmteam.js

--- commands/dmteam.js
+++ commands/dmteam.js
@@ -1,7 +1,6 @@
 exports.run = (client, message, args) => {
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    const oak = require("./oak.json");
+     const oak = require("./oak.json");
     const oak_tutorial_team = oak.tutorial_team
+    message.delete().catch(O_o=>{}); 
     message.mentions.users.first().send(oak_tutorial_team)
 }
