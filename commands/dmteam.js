a36f1b7682a2ab244c92664d059f61ebb8d09091
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-30 11:33:15 -0400

    Update dmteam.js

--- commands/dmteam.js
+++ commands/dmteam.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     const reminder = require("../data/reminder.json");
-    const team_reminder = reminder.reminder_team
+    const team_reminder = reminder.team
           message.delete().catch(O_o=>{}); 
           message.mentions.users.first().send(team_reminder)
 }
