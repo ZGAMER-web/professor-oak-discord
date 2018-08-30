28cd1bf3ad2bfe1bce414205a28fcf8d51f17048
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-08-30 10:10:38 -0400

    Update eastcarroll.js

--- gyms/eastcarroll.js
+++ gyms/eastcarroll.js
@@ -1,6 +1,8 @@
 module.exports.run = (client, message, args) => {
     const Discord = require("discord.js");
   const embed = new Discord.RichEmbed()
+    const location = require("../data/location.json");
+    const carrollton = location.location_carrollton
   
     .setTitle("East Carroll")
     
@@ -10,7 +12,7 @@ module.exports.run = (client, message, args) => {
 
     .setThumbnail("https://lh3.ggpht.com/TezUfbTmNcOZheSzLcq2Cfen7TSHCHAQKMS0nO0OYRh1X4Zzljer9QuPOouW5TAKPiPnI65WsCWcPQR5JreN")
     
-    .addField("**Location**", "Carrollton, GA")
+    .addField("**Location**", carrollton)
     .addField("**Access**", "Easy, Free Parking")
     .addField("**Ex Raid Eligable**", "Yes")
     .addField("**Commands**", "`eastcarroll` or `skatepark`")
