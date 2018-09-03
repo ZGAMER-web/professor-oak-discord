a3610c2d415b2444c148f233d825b3540f25c664
mrerecordholder <>
2018-09-03 11:38:54 -0400

    update v.3.2.7

--- gyms/skatepark.js
+++ gyms/skatepark.js
@@ -1,15 +1,13 @@
 module.exports.run = (client, message, args) => {
-    const Discord = require("discord.js");
+  const Discord = require("discord.js");
   const embed = new Discord.RichEmbed()
-  
-    .setTitle("East Carroll")
     
     .setColor(0x00AE86)
-    .setDescription("[CLICK HERE FOR DIRECTIONS](https://goo.gl/maps/q8qup3vJkrt)")
     .setFooter("© Professor Oak", "https://raw.githubusercontent.com/MrRecordHolder/professor-oak-discord/master/images/oak1.jpg")
 
+    .setTitle("East Carroll")
     .setThumbnail("https://lh3.ggpht.com/TezUfbTmNcOZheSzLcq2Cfen7TSHCHAQKMS0nO0OYRh1X4Zzljer9QuPOouW5TAKPiPnI65WsCWcPQR5JreN")
-    
+    .setDescription("[CLICK HERE FOR DIRECTIONS](https://goo.gl/maps/q8qup3vJkrt)")
     .addField("**Location**", "Carrollton, GA")
     .addField("**Access**", "Easy, Free Parking")
     .addField("**Ex Raid Eligable**", "Yes")
