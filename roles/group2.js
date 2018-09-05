ceb7666e47033b0936fac720a1c284e5fcfcebdc
mrerecordholder <>
2018-09-05 12:20:13 -0400

    update 3.0.1

--- roles/group2.js
+++ roles/group2.js
@@ -1,9 +1,4 @@
 exports.run = (client, message, args) => {
-    let role = message.guild.roles.find("name", "group 2");
     
-    let member = message.member;
-
-    member.addRole(role).catch(console.error);
-    
-    message.channel.send(message.author.toString() + ", I've added you to the **Moltres Day Group 2**. You can now chat with other Trainers with-in your raid group. Have fun Trainer and good luck!");
+    message.channel.send(message.author.toString() + ", Sorry Trainer! This group is full. Please select a different group or start your own. <#483421937641586699>");
 };
