2a1fe6a196a61709cee333d99b74f603bc4ed4b7
mrerecordholder <>
2018-09-04 20:53:54 -0400

    bug fix

--- roles/group1.js
+++ roles/group1.js
@@ -7,5 +7,4 @@ exports.run = (client, message, args) => {
     
     message.channel.send(message.author.toString() + ", I've added you to the **Moltres Day Group 1**. You can now chat with other Trainers with-in your raid group. Have fun Trainer and good luck!");
     message.delete(10000)
-    .catch ("Sorry Trainer, I could find that role.");
 };
