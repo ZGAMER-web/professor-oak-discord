44b587d3880b702021d0e64722e8abaaf770ea94
mrerecordholder <>
2018-09-04 20:57:08 -0400

    3.0.0 update

--- roles/group1.js
+++ roles/group1.js
@@ -6,5 +6,4 @@ exports.run = (client, message, args) => {
     member.addRole(role).catch(console.error);
     
     message.channel.send(message.author.toString() + ", I've added you to the **Moltres Day Group 1**. You can now chat with other Trainers with-in your raid group. Have fun Trainer and good luck!");
-    message.delete(10000)
 };
