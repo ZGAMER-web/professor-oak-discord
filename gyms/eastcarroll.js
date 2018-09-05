c01a7bdfdacbeefc43b9d8ca8c71f73f3294e45f
mrerecordholder <>
2018-09-05 19:20:35 -0400

    test

--- gyms/eastcarroll.js
+++ gyms/eastcarroll.js
@@ -7,7 +7,7 @@ module.exports.run = (client, message, args) => {
     .setColor(0x00AE86)
     .setFooter("© Professor Oak", "https://raw.githubusercontent.com/MrRecordHolder/professor-oak-discord/master/images/oak1.jpg")
 
-    .setTitle([version])
+    .setTitle("Name" + version)
     .setThumbnail("https://lh3.ggpht.com/TezUfbTmNcOZheSzLcq2Cfen7TSHCHAQKMS0nO0OYRh1X4Zzljer9QuPOouW5TAKPiPnI65WsCWcPQR5JreN")
     .setDescription("[CLICK HERE FOR DIRECTIONS](https://goo.gl/maps/q8qup3vJkrt)")
     .addField("**Location**", "Carrollton, GA")
