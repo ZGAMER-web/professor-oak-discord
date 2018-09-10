19fbfd0fc4475dac8ff13ee80c4d99d9cf6d3194
mrerecordholder <>
2018-09-10 19:35:04 -0400

    v.3.3.8 update

--- commands/exraid.js
+++ commands/exraid.js
@@ -13,7 +13,7 @@ module.exports.run = (client, message, args) => {
     .addField("How To Receive An Ex-Raid Pass",
       "According to my research, EX Raid passes are distributed by an in game alert at 2pm once per week to random Trainers that have completed raid(s) at gyms located at in-game parks or at sponsored locations such as Starbucks & Sprint. For the best chances, be sure your gym badge is ranked to gold and complete multiple raids. I have found all Ex-Raid locations for West GA.")
    
-    .addBlankField(true)
+    .addBlankField(false)
     .addField("**Bremen**", "N/A", true)
     .addField("**Carrollton**", "Starbucks, East Carroll", true)
     .addField("**Franklin**", "N/A", true)
