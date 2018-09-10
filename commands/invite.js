25ed31009e360c27978fbc545d56a39790831f80
mrerecordholder <>
2018-09-10 19:51:51 -0400

    v.3.3.8 update

--- commands/invite.js
+++ commands/invite.js
@@ -1,5 +1,4 @@
 exports.run = (client, message, args) => {
     message.guild.channels.get('483420033473576961').createInvite().then(invite =>
     message.channel.send(invite.url)
-    message.guild.channels.get("483831639575887873").send("A new server invite was created.");
 )};
