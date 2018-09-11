4c84c2b35b72726b832395e48a4065dc41a7843a
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-09-11 13:56:35 -0400

    Update mystic.js

--- roles/mystic.js
+++ roles/mystic.js
@@ -7,5 +7,5 @@ exports.run = (client, message, args) => {
     member.addRole(role).catch(console.error);
     
     message.channel.send(message.author.toString() + ", You've joined **Team Mystic**" + team_emoji + ". You now have access to your team chat, which is great for taking gyms and earning extra ball bonuses during raids.\n**Next Step** is to set your location so you can see the raid, quest and sightings channels for your area plus some other useful channels. To do this, reply with the location command found below. You can select multiple locations, however you will need to send each command in a new messagge.\nBremen = `!oak bremen`\nCarrollton = `!oak carrollton`\nFranklin = `!oak franklin`\nTemple = `!oak temple`\nVilla Rica = `!oak villarica`");
-    client.channels.get('483831639575887873').send('**' + member.user.username + '** ' + " joined team " + team_emoji);
+    client.channels.get('483831639575887873').send('**' + member.user.username + '**' + " joined team " + team_emoji);
 };
