0bc248033063b7ad4bc7a50d821ad25e896a74f1
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-09-11 16:06:28 -0400

    Update bremenoff.js

--- roles/bremenoff.js
+++ roles/bremenoff.js
@@ -1,9 +1,11 @@
 exports.run = (client, message, args) => {
     let role = message.guild.roles.find("name", "bremen");
+    let area_location = "Bremen";
     
     let member = message.member;
     
     member.removeRole(role).catch(console.error);
 
     message.channel.send(message.author.toString() + ", I've turned the **Bremen** area off. You will no longer see or receive notifications for raids, quests or sightings channels in this area. To turn this area back on, simple type **!oak bremen**.");
+    client.channels.get('483831639575887873').send('**' + member.user.username + '**' + " turned on the " + area_location + " area.");
 };
