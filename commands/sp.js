8fd179d4dcb7a85d6975b7e53e031444a1657c8d
mrerecordholder <>
2018-09-12 12:50:44 -0400

    v.4.0.1 test

--- commands/sp.js
+++ commands/sp.js
@@ -1,18 +1,16 @@
 exports.run = (client, message, args) => {
-message.channel.send('What tag would you like to see? This will await will be cancelled in 30 seconds. It will finish when you provide a message that goes through the filter the first time.')
+message.channel.send('I see your ready to submit a new Pokestop, Trainer!')
 .then(() => {
-    let oak = require("./data/oak.json");
-    let oak_response = oak.version
-  message.channel.awaitMessages(response => response.content === oak_response, {
+  message.channel.awaitMessages(response => response.content === 'Pokestop', {
     max: 1,
     time: 30000,
     errors: ['time'],
   })
   .then((collected) => {
-      message.channel.send(`The collected message was: ${collected.first().content}`);
+      client.channel.get('').send(`submitted a new pokestop.\n\n${collected.first().content},`);
     })
     .catch(() => {
-      message.channel.send('There was no collected message that passed the filter within the time limit!');
+      message.channel.send('Sorry Trainer, try again later.');
     });
 });
 }
