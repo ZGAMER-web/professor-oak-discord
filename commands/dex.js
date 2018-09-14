9b691d628b54c987b72777726fa6034ef6acccd1
mrerecordholder <>
2018-09-13 23:14:35 -0400

    test

--- commands/dex.js
+++ commands/dex.js
@@ -1,6 +1,5 @@
 exports.run = (client, message, args) => {
   let pokemon = args[0];
   let commandFile = require(`../pokedex/${pokemon}.js`);
-  commandFile.run(client, message, args)
-  .catch(message.channel.send("Sorry, Trainer. You may need to check your spelling or this Pokemon has not be added yet."));
+  commandFile.run(client, message, args).catch((e) => { message.channel.send("I didn't find that Pokemon, Trrainer. Please try again or ask a server mod for help."); });
 };
