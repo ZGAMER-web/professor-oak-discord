8500ebc9b1b918af0a716f293785ae1ac41554f6
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-09-14 11:41:15 -0400

    Update articuno.js

--- counters/articuno.js
+++ counters/articuno.js
@@ -2,6 +2,8 @@ module.exports.run = (client, message, args) => {
     const pokemon_data = require("../data/pokemon/articuno.json");
     const pokemon_name = pokemon_data.name
     const pokemon_dex = pokemon_data.dex
+    const pokemon_counter_01 = pokemon_data.counter_01
+    const pokemon_counter_01_moveset = pokemon_data.counter_01_moveset
     
     const Discord = require("discord.js");
     const embed = new Discord.RichEmbed()
@@ -11,8 +13,8 @@ module.exports.run = (client, message, args) => {
   
       .setTitle("**" + pokemon_dex + " " + pokemon_name + "**")
       .setThumbnail("https://github.com/MrRecordHolder/professor-oak-discord/blob/master/images/pokemon-icons/pokemon_icon_144_00.png?raw=true")
-      .setDescription("Legendary")
-      .addField("**Tyranitar**", "Smack Down / Stone Edge")
+      .setDescription("Recommended Trainers with counters at this level is 3, but you may need extra help.")
+      .addField(pokemon_counter_01, pokemon_counter_01_moveset)
   
       .setTimestamp()
     
