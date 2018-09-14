af0cdae4655be7aa5a07e6a59bc718091fe984f6
mrerecordholder <>
2018-09-13 22:23:34 -0400

    v.4.0.2 update

--- commands/dex.js
+++ commands/dex.js
@@ -1,5 +1,8 @@
 exports.run = (client, message, args) => {
   let pokemon = args[0];
   let commandFile = require(`../pokedex/${pokemon}.js`);
-  commandFile.run(client, message, args).catch(error => message.channel.send("Sorry Trainer."));
+  commandFile.run(client, message, args)
+  .catch((err) => {
+    message.channel.send('No results found!');
+ });
 }
