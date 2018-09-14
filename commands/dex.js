eeb9c2334797771d6ee738f47bee3dfc8c098de5
mrerecordholder <>
2018-09-13 23:46:09 -0400

    test

--- commands/dex.js
+++ commands/dex.js
@@ -1,6 +1,9 @@
 exports.run = (client, message, args) => {
   let pokemon = args[0];
   let commandFile = require(`../pokedex/${pokemon}.js`);
-  commandFile.run(client, message, args)
-  if(commandFile == null)console.error("This Pokemon has not be added yet.");
+  try {
+    commandFile.run(client, message, args)
+  } catch (err) {
+    message.channel.send("No Pokemon found. Check your spelling and try again...");
+  }
 };
