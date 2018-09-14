ec9d176aad31f33e6c97f4c558a8e725518ac2b6
mrerecordholder <>
2018-09-13 22:59:31 -0400

    update

--- commands/dex.js
+++ commands/dex.js
@@ -2,5 +2,5 @@ exports.run = (client, message, args) => {
   let pokemon = args[0];
   let commandFile = require(`../pokedex/${pokemon}.js`);
   commandFile.run(client, message, args)
-  if (err) return message.channel.send("Sorry, Trainer. You may need to check your spelling or this Pokemon has not be added yet.");
+  .catch(message.channel.send("Sorry, Trainer. You may need to check your spelling or this Pokemon has not be added yet."));
 };
