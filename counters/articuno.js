b7be95bf048a22fe035d4a0fa8e0455fc33460d9
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-09-14 13:54:26 -0400

    Update articuno.js

--- counters/articuno.js
+++ counters/articuno.js
@@ -2,8 +2,6 @@ module.exports.run = (client, message, args) => {
     const pokemon_data = require("../data/pokemon/articuno.json");
     const pokemon_name = pokemon_data.name
     const pokemon_dex = pokemon_data.dex
-    const pokemon_counter_01 = pokemon_data.counter_01
-    const pokemon_counter_01_moveset = pokemon_data.counter_01_moveset
     
     const Discord = require("discord.js");
     const embed = new Discord.RichEmbed()
