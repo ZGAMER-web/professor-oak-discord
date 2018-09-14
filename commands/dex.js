743bada89826b260022854ca02f6a53d663f76da
mrerecordholder <>
2018-09-13 21:06:06 -0400

    test

--- commands/dex.js
+++ commands/dex.js
@@ -1,17 +1,5 @@
-client.on("message", message => {
-    if (message.author.bot) return;
-    if(message.content.indexOf(config.prefix) !== 0) return;
-     
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-    
-   
-    //CHECK FOR COMMANDS
-    
-  try {
-      let commandFile = require(`../pokemon/${command}.js`);
-      commandFile.run(client, message, args);
-    } catch (err) {
-      console.error("No Pokemon could be found, Sorry!");
-    }    
-});
+exports.run = (client, message, args) => {
+  let pokemon = args[0];
+  let commandFile = require(`../pokemon/${pokemon}.js`);
+  commandFile.run(client, message, args);
+}
