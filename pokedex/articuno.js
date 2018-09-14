e2066406d1a50bc103b1587bf93c3d0933752c43
mrerecordholder <>
2018-09-13 21:16:18 -0400

    embed test

---
+++ pokedex/articuno.js
@@ -0,0 +1,19 @@
+module.exports.run = (client, message, args) => {
+    const Discord = require("discord.js");
+    const embed = new Discord.RichEmbed()
+      
+      .setColor(0x00AE86)
+      .setFooter("© Professor Oak", "https://raw.githubusercontent.com/MrRecordHolder/professor-oak-discord/master/images/oak1.jpg")
+  
+      .setTitle("Articuno")
+      .setThumbnail("https://lh3.ggpht.com/TezUfbTmNcOZheSzLcq2Cfen7TSHCHAQKMS0nO0OYRh1X4Zzljer9QuPOouW5TAKPiPnI65WsCWcPQR5JreN")
+      .setDescription("[CLICK HERE FOR DIRECTIONS](https://goo.gl/maps/q8qup3vJkrt)")
+      .addField("**Location**", "Carrollton, GA")
+      .addField("**Access**", "Easy, Free Parking")
+      .addField("**Ex Raid Eligable**", "Yes")
+      .addField("**Commands**", "`eastcarroll` or `skatepark`")
+  
+      .setTimestamp()
+    
+      message.channel.send({embed});
+    }
