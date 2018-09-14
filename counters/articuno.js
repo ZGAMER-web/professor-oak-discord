defdb41cdeff00841c2010035b3a31e546300da7
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-09-14 11:20:14 -0400

    Update articuno.js

--- counters/articuno.js
+++ counters/articuno.js
@@ -1,6 +1,7 @@
 module.exports.run = (client, message, args) => {
     const pokemon_data = require("../data/pokemon/${command}.json");
     const pokemon_name = pokemon_data.name
+    const pokemon_dex = pokemon_data.dex
     
     const Discord = require("discord.js");
     const embed = new Discord.RichEmbed()
@@ -8,7 +9,7 @@ module.exports.run = (client, message, args) => {
       .setColor(0x00AE86)
       .setFooter("© Professor Oak", "https://raw.githubusercontent.com/MrRecordHolder/professor-oak-discord/master/images/oak1.jpg")
   
-      .setTitle(pokemon_name)
+      .setTitle("**" + pokemon_dex + " " + pokemon_name + "**")
       .setThumbnail("https://github.com/MrRecordHolder/professor-oak-discord/blob/master/images/pokemon-icons/pokemon_icon_144_00.png?raw=true")
       .setDescription("Legendary")
       .addField("**Tyranitar**", "Smack Down / Stone Edge")
