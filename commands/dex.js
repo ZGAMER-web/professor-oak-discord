0298d2863dfc3a90d8ec6345cdf807579175f72e
mrerecordholder <>
2018-09-13 22:19:29 -0400

    v.4.0.2 update

--- commands/dex.js
+++ commands/dex.js
@@ -1,6 +1,5 @@
 exports.run = (client, message, args) => {
-  let shiny = args[0];
-  let pokemon = args[1];
-  let commandFile = require(`../pokedex/${shiny}${pokemon}.js`);
-  commandFile.run(client, message, args);
+  let pokemon = args[0];
+  let commandFile = require(`../pokedex/${pokemon}.js`);
+  commandFile.run(client, message, args).catch(error => message.channel.send("Sorry Trainer."));
 }
