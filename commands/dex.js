b17d9ad7ef4f1c3a225eec16a941c5c1020199bb
mrerecordholder <>
2018-09-13 23:39:24 -0400

    test

--- commands/dex.js
+++ commands/dex.js
@@ -2,5 +2,11 @@ exports.run = (client, message, args) => {
   let pokemon = args[0];
   let commandFile = require(`../pokedex/${pokemon}.js`);
 
-  commandFile.run(client, message, args).catch("No Pokemon found. Try again, be sure to check your spelling.")
+
+  if (commandFile == null) {
+    message.channel.send("No Pokemon was found. Check your spelling...")
+}
+
+
+  commandFile.run(client, message, args)
 };
