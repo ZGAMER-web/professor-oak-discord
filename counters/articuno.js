c18dbb2480b7e868a17fb7e5b732eabbaa4b9e2f
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-09-14 14:08:37 -0400

    Update articuno.js

--- counters/articuno.js
+++ counters/articuno.js
@@ -9,10 +9,10 @@ module.exports.run = (client, message, args) => {
       .setColor(0x00AE86)
       .setFooter("© Professor Oak", "https://raw.githubusercontent.com/MrRecordHolder/professor-oak-discord/master/images/oak1.jpg")
   
-      .setTitle("**" + pokemon_dex + " " + pokemon_name + "**")
+      .setTitle(pokemon_dex + " " + pokemon_name)
       .setThumbnail("https://github.com/MrRecordHolder/professor-oak-discord/blob/master/images/pokemon-icons/pokemon_icon_144_00.png?raw=true")
       .setDescription("Legendary")
-      .addField("**Tyranitar**", "Smack Down / Stone Edge")
+      .addField("**Attack**", "Carrollton, GA")
   
       .setTimestamp()
     
