813edf5c1b3873db1727fc7e7ab0c34df99ed947
mrerecordholder <>
2018-09-14 22:22:01 -0400

    test

---
+++ commands/nickname.js
@@ -0,0 +1,5 @@
+exports.run = (client, message, args) => {
+if (message.content.includes('changeNick')) {
+    if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
+    message.member.setNickname(message.content.replace('changeNick ', ''));
+}};
