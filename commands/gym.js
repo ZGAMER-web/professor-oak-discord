c0ea0d228edbc653e41ea079e0d4834ddbae7ae2
mrerecordholder <>
2018-09-15 13:13:05 -0400

    update

--- commands/gym.js
+++ commands/gym.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-    let gym_name = args[0];
-    let commandFile = require(`../commands/gyms/${gym_name}.js`);
+    let gym_name = args.join(" ");
+    let commandFile = require(`../gyms/${gym_name}.js`);
     commandFile.run(client, message, args);
   }
