a73a4eee0c691f4cce354a19323f247aeba957e3
mrerecordholder <>
2018-09-15 16:27:18 -0400

    update

--- commands/gym.js
+++ commands/gym.js
@@ -2,4 +2,8 @@ exports.run = (client, message, args) => {
     let gym_name = args.join(" ");
     let commandFile = require(`../new-gyms/${gym_name}.js`);
     commandFile.run(client, message, args);
+    message.reply("hello")
+    .then(msg => {
+      message.delete(10000).catch(O_o=>{}); 
+    })
   }
--- commands/news.js
+++ commands/news.js
@@ -2,5 +2,4 @@ exports.run = (client, message, args) => {
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
     client.channels.get("451467154944950282").send(sayMessage);
-    client.channels.get('483831639575887873').send('**' + member.user.username + '**' + " posted in <#451467154944950282> using my `!oak news` command.");
 }
