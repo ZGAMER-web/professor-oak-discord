5d6f826741f6e4c867f0d2970a696decf6b749e0
mrerecordholder <>
2018-09-15 09:40:41 -0400

    test

--- gyms/eastcarroll.js
+++ gyms/eastcarroll.js
@@ -1,11 +1,14 @@
 module.exports.run = (client, message, args) => {
+  const pokemon = require("../data/gyms/eastcarroll.json");
+  const name = gym.name
+    
   const Discord = require("discord.js");
   const embed = new Discord.RichEmbed()
     
     .setColor(0x00AE86)
     .setFooter("© Professor Oak", "https://raw.githubusercontent.com/MrRecordHolder/professor-oak-discord/master/images/oak1.jpg")
 
-    .setTitle("East Carroll")
+    .setTitle(name)
     .setThumbnail("https://lh3.ggpht.com/TezUfbTmNcOZheSzLcq2Cfen7TSHCHAQKMS0nO0OYRh1X4Zzljer9QuPOouW5TAKPiPnI65WsCWcPQR5JreN")
     .setDescription("[CLICK HERE FOR DIRECTIONS](https://goo.gl/maps/q8qup3vJkrt)")
     .addField("**Location**", "Carrollton, GA")
