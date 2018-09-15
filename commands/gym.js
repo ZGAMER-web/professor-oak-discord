03aa8dc725c64d790a2fedfa404d5b096d2353ce
mrerecordholder <>
2018-09-15 16:31:09 -0400

    test

--- commands/gym.js
+++ commands/gym.js
@@ -1,9 +1,11 @@
 exports.run = (client, message, args) => {
-    let gym_name = args.join(" ");
+  message.reply("I found the following directions.")
+  .then(msg => {
+    msg.delete(10000)
+  })
+  
+  let gym_name = args.join(" ");
     let commandFile = require(`../new-gyms/${gym_name}.js`);
     commandFile.run(client, message, args);
-    message.reply("hello")
-    .then(msg => {
-      message.delete(10000).catch(O_o=>{}); 
-    })
+    commandFile.delete(10000)
   }
