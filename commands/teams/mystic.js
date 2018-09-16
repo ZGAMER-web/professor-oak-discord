61c87ed90dc82f395880c1a69e4df824f8641c2e
mrerecordholder <>
2018-09-16 18:43:18 -0400

    update

--- commands/teams/mystic.js
+++ commands/teams/mystic.js
@@ -1,10 +1,13 @@
 exports.run = (client, message, args) => {
     let role = message.guild.roles.find("name", "mystic");
     let team_emoji = client.emojis.find("name", "mystic");
+    let team = require("../data/teams.json")
+    let join_team_description = team.join_team_description
 
     let member = message.member;
     
     member.addRole(role).catch(console.error);
+    message.reply(team_emoji);
     
   const Discord = require("discord.js");
   const embed = new Discord.RichEmbed()
@@ -12,19 +15,17 @@ exports.run = (client, message, args) => {
     .setTitle("**You've joined Team Mystic**")
     
     .setColor(0x00AE86)
-    .setDescription("You now have access to your team chat and a few various other channels. Now you need to set your location. To do this, reply back with one of the commands listed below.")
+    .setDescription(join_team_description)
     .setFooter("© Professor Oak", "https://github.com/MrRecordHolder/professor-oak-discord/blob/master/images/oak1.jpg?raw=true")
     .setThumbnail("https://github.com/MrRecordHolder/professor-oak-discord/blob/master/images/logos/team_mystic.png?raw=true")
     
-    .addField("Bremen", "`!oak area bremen`")
-    .addField("Carrollton", "`!oak area carrollton`")
-    .addField("Franklin", "`!oak area franklin`")
-    .addField("Temple", "`!oak area temple`")
-    .addField("Villa Rica", "`!oak area villa rica`")
+    .addField("Bremen", "`!oak location bremen`")
+    .addField("Carrollton", "`!oak location carrollton`")
+    .addField("Franklin", "`!oak location franklin`")
+    .addField("Temple", "`!oak location temple`")
+    .addField("Villa Rica", "`!oak location villa rica`")
 
     .setTimestamp()
   
     message.channel.send({embed});
-
-    message.reply("");
 };
