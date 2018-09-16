0c2973d899c48f33243418956d98c5466362fa72
mrerecordholder <>
2018-09-16 13:29:18 -0400

    test

--- bot.js
+++ bot.js
@@ -54,12 +54,12 @@ client.on("message", (message) => {
 if( swearWords.some(word => message.content.includes(word)) ) {
   message.reply("Oh no you said a bad word!!!").then(sentMessage =>{
     sentMessage.delete(5000)
-  member.guild.channels.get('483831639575887873').send('**' + member.user.username + '**, used a prohibited word.');
+  client.channels.get('483831639575887873').send('**' + member.user.username + '**, used a prohibited word.');
   })
   // Or just do message.delete();
   setTimeout(function(){ 
     message.delete()
- }, 5000); //time in milliseconds
+ }, 100); //time in milliseconds
 }
 });
 
