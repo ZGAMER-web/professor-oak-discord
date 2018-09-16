243eb0619dfefca9a06cdce36e67c2e5cc8e4199
mrerecordholder <>
2018-09-16 13:36:57 -0400

    update

--- bot.js
+++ bot.js
@@ -52,14 +52,12 @@ client.on("message", (message) => {
   }
   const swearWords = ["darn", "shucks", "frak", "shite"];
 if( swearWords.some(word => message.content.includes(word)) ) {
-  message.reply("Oh no you said a bad word!!!").then(sentMessage =>{
+  message.reply("I've deleted that last message.").then(sentMessage =>{
     sentMessage.delete(5000)
   })
-  client.channels.get('483831639575887873').send('**' + member.user.username + '**, used a prohibited word.');
-  // Or just do message.delete();
   setTimeout(function(){ 
     message.delete()
- }, 1000); //time in milliseconds
+ }, 5000);
 }
 });
 
