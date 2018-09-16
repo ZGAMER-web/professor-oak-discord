44e37a8abf5fca6dd3465fc2879eb3ee11ebd337
mrerecordholder <>
2018-09-16 12:58:21 -0400

    auto delete test

--- commands/dex.js
+++ commands/dex.js
@@ -1,5 +1,7 @@
 exports.run = (client, message, args) => {
   let pokemon = args.join(" ");
   let commandFile = require(`../pokedex/${pokemon}.js`);
-    commandFile.run(client, message, args)
+    commandFile.run(client, message, args).then(sentMessage => {
+      sentMessage.delete(10000)
+    })
 };
