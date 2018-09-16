5a994a7da7c635532842f27a1bce5087fc14ef35
mrerecordholder <>
2018-09-15 22:14:49 -0400

    update

--- commands/counters.js
+++ commands/counters.js
@@ -1,6 +1,5 @@
 exports.run = (client, message, args) => {
-  let args = message.content.shift().toLowerCase();  
-  let pokemon = args.join(" ");
+    let pokemon = args.join(" ");
     let commandFile = require(`../counters/${pokemon}.js`);
     commandFile.run(client, message, args);
   }
