ff62cba1bd36ce00c586c9bc205d427fa42f8a76
mrerecordholder <>
2018-09-16 19:43:44 -0400

    update

--- commands/gyms/east carroll.js
+++ commands/gyms/east carroll.js
@@ -13,6 +13,8 @@ module.exports.run = (client, message, args) => {
     const oak_version = oak.version
     const oak_avi = oak.avi
     const message_delete_time_default = oak.message_delete_time_default
+
+    message.delete(message_delete_time_default)
       
     const Discord = require("discord.js");
     const embed = new Discord.RichEmbed()
@@ -32,7 +34,5 @@ module.exports.run = (client, message, args) => {
     
       message.channel.send({embed}).then(sentMessage => {
         sentMessage.delete(message_delete_time_default)
-      }).then(message => {
-        message.delete(message_delete_time_default)
       })
     }
