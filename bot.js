fed1353aacb3bacfb9ca32db13a56f1cc7baff88
mrerecordholder <>
2018-09-16 21:36:16 -0400

    fix

--- bot.js
+++ bot.js
@@ -56,7 +56,7 @@ client.on("message", (message) => {
       sentMessage.delete(20000)
     })
     message.delete(3000)
-    client.channel.get('483831639575887873').send(member.user + " said a `swearWord`.")
+    client.channels.get('483831639575887873').send(member.user + " said a `swearWord`.")
   }
 });
 
