ea58ee3df7b2a962e9898b159b04f9161ce74242
mrerecordholder <>
2018-09-19 19:53:39 -0400

    restructure/logging

--- bot.js
+++ bot.js
@@ -7,11 +7,39 @@ const responderObject = require('./data/responder.json');
 const oak = require("./data/oak.json");
 const version = oak.version
 
+// WAKE UP PROFESSOR OAK!
+client.on("ready", () => {
+  message.channel.get('492120328160542720').send(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  client.user.setGame(prefix + " " + version);
+});
+
+/// WELCOME NEW TRAINER
+const oak_welcome = oak.welcome
+const oak_introduction = oak.introduction
+const oak_set_team = oak.set_team
+const oak_help = oak.help
+const oak_trainer_joined = oak.trainer_joined
+
+client.on('guildMemberAdd', member => {
+  member.guild.channels.get('483420033473576961').send(member.user + ", " + oak_welcome + " " + oak_introduction + " " + oak_set_team + "\n" + oak_help).then(sentMessage => {
+    sentMessage.delete(300000)
+  }) 
+  member.guild.channels.get('483831639575887873').send('**' + member.user.username + '**, ' + oak_trainer_joined);
+});
+
+/// TRAINER LEFT THE SEVER
+const oak_trainer_left = oak.trainer_left
+
+client.on('guildMemberRemove', member => {
+  member.guild.channels.get('483831639575887873').send('**' + member.user.username + '**, ' + oak_trainer_left);
+});
+
+
 // LOADS & RUNS THE COMMAND FOLDER
 client.on("message", message => {
   if (message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
-  client.user.setGame(prefix + " " + version);
+  
    
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
@@ -58,28 +86,6 @@ client.on("message", (message) => {
     message.delete(3000)
     client.channels.get('483831639575887873').send(message.author.toString() + " used a `swearWord`. I've let them know not to use this type of language on this server.")
   }
-});
-
-
-/// WELCOME NEW TRAINER
-const oak_welcome = oak.welcome
-const oak_introduction = oak.introduction
-const oak_set_team = oak.set_team
-const oak_help = oak.help
-const oak_trainer_joined = oak.trainer_joined
-
-client.on('guildMemberAdd', member => {
-  member.guild.channels.get('483420033473576961').send(member.user + ", " + oak_welcome + " " + oak_introduction + " " + oak_set_team + "\n" + oak_help).then(sentMessage => {
-    sentMessage.delete(300000)
-  }) 
-  member.guild.channels.get('483831639575887873').send('**' + member.user.username + '**, ' + oak_trainer_joined);
-});
-
-/// TRAINER HAS LEFT THE SEVER
-const oak_trainer_left = oak.trainer_left
-
-client.on('guildMemberRemove', member => {
-  member.guild.channels.get('483831639575887873').send('**' + member.user.username + '**, ' + oak_trainer_left);
 });
 
 client.login(process.env.BOT_TOKEN);
