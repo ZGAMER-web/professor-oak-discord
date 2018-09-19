484fb5f12d3aa05658c70589b89c1a07ff9a3496
MrRecordHolder <38539569+MrRecordHolder@users.noreply.github.com>
2018-09-19 09:31:36 -0400

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send("New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!").catch(console.error);
+    message.channel.send("Server name = ${client.guild.name").catch(console.error);
 }
