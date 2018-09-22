d4f728c11eabebdb2a426cafb3d105f51d23fb02
mrerecordholder <>
2018-09-22 19:57:54 -0400

    bug fix

--- commands/gyms/contemporary church.js
+++ commands/gyms/contemporary church.js
@@ -1,5 +1,5 @@
 module.exports.run = (client, message, args) => {
-  const gym = require("../../data/gyms/contemporary_church.json");
+  const gym = require("../../data/gyms/contemporary-church.json");
   const gym_name = gym.name
   const gym_picture = gym.picture
   const gym_map = gym.map
