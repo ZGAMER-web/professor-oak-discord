dccde47bfa6a487a93236b740f41fce35b33d6e7
mrerecordholder <>
2018-09-22 21:33:01 -0400

    update

--- commands/gym.js
+++ commands/gym.js
@@ -1,5 +1,7 @@
 exports.run = (client, message, args) => {  
   let gym = args.join(" ");
   let commandFile = require(`../commands/gyms/${gym}.js`);
-    commandFile.run(client, message, args);
+  commandFile.run(client, message, args).catch(() => {
+    message.channel.send('There was no collected message that passed the filter within the time limit!');
+  });
 };
