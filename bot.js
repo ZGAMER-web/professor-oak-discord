d3adb619178edf99dd9eb4501f709526f92ebf2d
mrerecordholder <>
2018-09-24 12:48:19 -0400

    update 4.1.7
    - resolved cache issues in console
    - removed multiple server accessibility
    - added spoofer warning response
    - cleaned up responses

--- bot.js
+++ bot.js
@@ -7,24 +7,10 @@ const responderObject = require('./data/responder.json');
 const oak = require("./data/oak.json");
 const version = oak.version 
 
-
 // WAKE UP PROFESSOR OAK!
 client.on("ready", () => {
-  console.log(`I have restarted my database. I have reconized ${client.users.size} total Trainers, ${client.channels.size} total channels in ${client.guilds.size} servers.`)
-  client.channels.find("name", "bot-log").send(`**UPDATE: ${version}** I have restarted my database. I have reconized ${client.users.size} total Trainers, ${client.channels.size} total channels in ${client.guilds.size} servers.`);
-  client.user.setGame(prefix + " " + version);
-});
-
-client.on("guildCreate", guild => {
-  guild.createChannel('professor-oak', 'text');
-  guild.createChannel('bot-log', 'text');
-  /// NEED TO RUN A MESSAGE FOR THE PROFESSOR OAK CHANNEL HERE
-  client.channels.get('483831639575887873').send(`**New Server:** ${guild.memberCount} Trainers.\n${guild.name} - (id: ${guild.id}).`);
-  client.user.setGame(prefix + " " + version);
-});
-
-client.on("guildDelete", guild => {
-  client.channels.get('483831639575887873').send(`**Left Server:** I have now serving ${client.users.size} total Trainers in ${client.guilds.size} servers.\n${guild.name} (id: ${guild.id}).`)
+  console.log(`I have restarted my database. I have reconized ${client.users.size} total Trainers, ${client.channels.size} total channels in ${client.guilds.size} servers.`).catch(console.error);
+  client.channels.find("name", "bot-log").send(`**UPDATE: ${version}** I have restarted my database. I have reconized ${client.users.size} total Trainers, ${client.channels.size} total channels in ${client.guilds.size} servers.`).catch(console.error);
   client.user.setGame(prefix + " " + version);
 });
 
@@ -37,16 +23,16 @@ const oak_trainer_joined = oak.trainer_joined
 
 client.on('guildMemberAdd', member => {
   member.guild.channels.get('483420033473576961').send(member.user + ", " + oak_welcome + " " + oak_introduction + "\n" + oak_set_team + "\n" + oak_help).then(sentMessage => {
-    sentMessage.delete(300000)
+    sentMessage.delete(300000).catch(console.error);
   }) 
-  member.guild.channels.get('483831639575887873').send(member.user + ', ' + oak_trainer_joined);
+  member.guild.channels.get('483831639575887873').send(member.user + ', ' + oak_trainer_joined).catch(console.error);
 });
 
 /// TRAINER LEFT THE SEVER
 const oak_trainer_left = oak.trainer_left
 
 client.on('guildMemberRemove', member => {
-  member.guild.channels.get('483831639575887873').send('**' + member.user + '**, ' + oak_trainer_left);
+  member.guild.channels.get('483831639575887873').send('**' + member.user + '**, ' + oak_trainer_left).catch(console.error);
 });
 
 
@@ -65,9 +51,9 @@ client.on("message", message => {
     commandFile.run(client, message, args);
   } catch (err) {
       console.error("No commands found...");
-      message.reply("Sorry Trainer! I couldn't find any data using that command. Be sure you start each new message using **!oak** followed by a space then a command. Be sure to __only__ use lowercase letters. See some examples above as some commands and arguments may have recently changed.").then(sentMessage =>{
-        sentMessage.delete(30000)
-        message.delete(30000)
+      message.reply("Sorry Trainer! I couldn't find any data using that command. Start each new message using **!oak**, then a space, then a command for me to understand you properly. See all my commands in my channel.").then(sentMessage =>{
+        sentMessage.delete(30000).catch(console.error);
+        message.delete(30000).catch(console.error);
       });
    }
 });
@@ -80,17 +66,21 @@ client.on("message", (message) => {
   const swearWords = ["shit", "Shit", "fuck", "Fuck", "dick", "Dick", "pussy", "Pussy", "bitch", "Bitch", "cunt", "Cunt"];
   if( swearWords.some(word => message.content.includes(word)) ) {
     message.reply("Please refrain from using that type of language on this server Trainer. I'm gonna delete that last message... I've informed the server mods of this action.").then(sentMessage =>{
-      sentMessage.delete(20000)
+      sentMessage.delete(20000).catch(console.error);
     })
-    message.delete(3000)
-    client.channels.get('483831639575887873').send(message.author.toString() + " used a `swearWord`. I've let them know not to use this type of language on this server.")
+    message.delete(3000).catch(console.error);
+    client.channels.get('483831639575887873').send(message.author.toString() + " used a `swearWord`. I've let them know not to use this type of language on this server.").catch(console.error);
   }
   const oakHelp = ["!Oak", "! Oak", "! oak"];
   if( oakHelp.some(word => message.content.includes(word)) ) {
     message.reply("Start each new message using **!oak**, then a space, then a command for me to understand you properly.").then(sentMessage =>{
-      sentMessage.delete(20000)
+      sentMessage.delete(30000).catch(console.error);
     })
-    message.delete(20000)
+    message.delete(30000).catch(console.error);
+  }
+  const Spoof = ["spoof", "spoofer", "Spoof", "Spoofer", "spoofers", "Spoofers", "tutu app", "tutu", "Tutu app", "Tutu App"];
+  if( Spoof.some(word => message.content.includes(word)) ) {
+    message.reply("This is a break Pokemon Go's TOS policy and risks your account of being banned. However, to each is own.").catch(console.error);
   }
 });
 
