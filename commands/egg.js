5b83ce1826f707a7a0f9e1fc8198abfa36567612
mrerecordholder <>
2018-09-26 23:12:26 -0400

    update

---
+++ commands/egg.js
@@ -0,0 +1,36 @@
+module.exports.run = (client, message, args) => {
+    
+  let eggLevel = args[0];  
+    
+    let gymData = args[1];
+    const gym = require(`../data/gyms/${gymData}.json`);
+    const gym_name = gym.name
+    const gym_map = gym.map
+    const gym_ex_raid = gym.ex_raid
+    const gym_access = gym.access
+  
+    let raidTimer = args[2];
+    const oak = require("../data/oak.json")
+    const oak_name = oak.name
+    const oak_version = oak.version
+    const oak_avi = oak.avi
+    const deleteTimer = oak.deleteTimer_default
+  
+    message.delete(deleteTimer).catch(console.error);
+    
+    const Discord = require("discord.js");
+    const embed = new Discord.RichEmbed()
+      
+    .setColor(0x00AE86)
+    .setFooter(oak_name + " " + oak_version, oak_avi)
+  
+    .setTitle("**" + eggLevel + " Raid!**" + "\n@" + gym_name)
+    .setThumbnail("https://github.com/MrRecordHolder/professor-oak-discord/blob/master/images/assets/ic_raid_egg_" + eggLevel + ".png?raw=true")
+    .setDescription("Hatch: " + raidTimer + " minutes left\n" + "Ex Raid Eligable: " + gym_ex_raid + "\nGym Access: " + gym_access + "\n[GET DIRECTIONS](" + gym_map + ")")
+    
+    .setTimestamp()
+  
+    client.channels.get("494640269455786003").send({embed});
+  }
+  
+  
